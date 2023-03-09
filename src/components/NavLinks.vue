9ecddb2b156341ca6ed30d1fc6df8699f98804d0
Nikita Androsenko <varzaid@gmail.com>
2023-03-09 10:55:59 +0300

    Навигация перенесена в компонент NavLinks.vue

---
+++ src/components/NavLinks.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-drawer>
+    <q-scroll-area
+      style="
+        height: calc(100% - 160px);
+        margin-top: 160px;
+        border-right: 1px solid #ddd;
+      "
+    >
+      <q-list padding class="menu-list">
+        <q-item to="/" exact clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="format_list_bulleted" />
+          </q-item-section>
+          <q-item-section> Todo </q-item-section>
+        </q-item>
+        <q-item to="/about" exact clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="info_outline" />
+          </q-item-section>
+          <q-item-section> About </q-item-section>
+        </q-item>
+        <q-item to="/help" exact clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="help_outline" />
+          </q-item-section>
+          <q-item-section> Help </q-item-section>
+        </q-item>
+        <q-item to="/contacts" exact clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="mail_outline" />
+          </q-item-section>
+          <q-item-section> Contacts </q-item-section>
+        </q-item>
+      </q-list>
+    </q-scroll-area>
+    <q-img
+      class="absolute-top avatar-bg-shadow"
+      src="~/assets/dungeon.jpg"
+      style="height: 160px"
+    >
+      <div class="absolute-bottom bg-transparent" style="z-index: 1">
+        <q-avatar size="56px" class="q-mb-sm">
+          <img src="~/assets/avatar-nik.jpg" />
+        </q-avatar>
+        <div class="text-weight-bold">Nikita Androsenko</div>
+        <div>@nikita</div>
+      </div>
+    </q-img>
+  </q-drawer>
+</template>
+
+<script>
+export default {
+  name: "NavLinks",
+};
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,66 +11,14 @@
       <q-img class="header-img absolute-top" src="~/assets/dragon.jpg" />
     </q-header>
 
-    <q-drawer
-      show-if-above
+    <NavLinks
       v-model="leftDrawerOpen"
+      show-if-above
       side="left"
       elevated
       :width="250"
       :breakpoint="600"
-    >
-      <q-scroll-area
-        style="
-          height: calc(100% - 160px);
-          margin-top: 160px;
-          border-right: 1px solid #ddd;
-        "
-      >
-        <q-list padding class="menu-list">
-          <q-item to="/" exact clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon name="format_list_bulleted" />
-            </q-item-section>
-            <q-item-section> Todo </q-item-section>
-          </q-item>
-          <q-item to="/about" exact clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon name="info_outline" />
-            </q-item-section>
-            <q-item-section> About </q-item-section>
-          </q-item>
-          <q-item to="/help" exact clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon name="help_outline" />
-            </q-item-section>
-            <q-item-section> Help </q-item-section>
-          </q-item>
-          <q-item to="/contacts" exact clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon name="mail_outline" />
-            </q-item-section>
-            <q-item-section> Contacts </q-item-section>
-          </q-item>
-        </q-list>
-      </q-scroll-area>
-      <q-img
-        class="absolute-top avatar-bg-shadow"
-        src="~/assets/dungeon.jpg"
-        style="height: 160px"
-      >
-        <div class="absolute-bottom bg-transparent" style="z-index: 1">
-          <q-avatar size="56px" class="q-mb-sm">
-            <img src="~/assets/avatar-nik.jpg" />
-          </q-avatar>
-          <div class="text-weight-bold">Nikita Androsenko</div>
-          <div>@nikita</div>
-        </div>
-      </q-img>
-    </q-drawer>
-
-    <q-drawer v-model="rightDrawerOpen" side="right" overlay bordered>
-      <!-- drawer content -->
-    </q-drawer>
+    />
 
     <q-page-container>
       <router-view v-slot="{ Component }">
@@ -100,12 +48,20 @@
 </template>
 
 <script>
-import { watch, defineComponent, ref } from "vue";
+import { watch, ref } from "vue";
 import { useQuasar, date } from "quasar";
 import gsap from "gsap";
+import NavLinks from "src/components/NavLinks.vue";
 
-export default defineComponent({
+export default {
   name: "MainLayout",
+  components: { NavLinks },
+  computed: {
+    todaysDate() {
+      const timeStamp = Date.now();
+      return date.formatDate(timeStamp, "dddd DD-MM-YYYY");
+    },
+  },
   setup() {
     const leftDrawerOpen = ref(false);
     const rightDrawerOpen = ref(false);
@@ -131,32 +87,24 @@ export default defineComponent({
       },
       drawer: ref(false),
       value: ref("Light"),
+      enter(el) {
+        gsap.fromTo(
+          el,
+          {
+            y: -100,
+            opacity: 0,
+          },
+          {
+            delay: 0.5,
+            duration: 1,
+            y: 0,
+            opacity: 1,
+          }
+        );
+      },
     };
   },
-  computed: {
-    todaysDate() {
-      const timeStamp = Date.now();
-      return date.formatDate(timeStamp, "dddd DD-MM-YYYY");
-    },
-  },
-  methods: {
-    enter(el) {
-      gsap.fromTo(
-        el,
-        {
-          y: -100,
-          opacity: 0,
-        },
-        {
-          delay: 0.5,
-          duration: 1,
-          y: 0,
-          opacity: 1,
-        }
-      );
-    },
-  },
-});
+};
 </script>
 
 <style lang="scss">
