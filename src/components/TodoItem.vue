d8343c080727280decf9a73f4cd30c593e206822
Nikita Androsenko <varzaid@gmail.com>
2023-03-10 16:15:57 +0300

    Организация отображения компонентов в компоненте Todo

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,30 +1,32 @@
 <template>
-  <q-item
-    @click="toggleDone"
-    clickable
-    :class="{ 'done bg-red-1': task_data.done }"
-    v-ripple
-  >
-    <q-item-section avatar>
-      <q-checkbox
-        class="no-pointer-events"
-        v-model="task_data.done"
-        color="primary"
-      />
-    </q-item-section>
-    <q-item-section>
-      <q-item-label>{{ task_data.title }}</q-item-label>
-    </q-item-section>
-    <q-item-section v-if="task_data.done" side>
-      <q-btn
-        @click.stop="deleteTask"
-        dense
-        round
-        color="primary"
-        icon="delete"
-      />
-    </q-item-section>
-  </q-item>
+  <q-list>
+    <q-item
+      @click="toggleDone"
+      clickable
+      :class="{ 'done bg-red-1': task_data.done }"
+      v-ripple
+    >
+      <q-item-section avatar>
+        <q-checkbox
+          class="no-pointer-events"
+          v-model="task_data.done"
+          color="primary"
+        />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ task_data.title }}</q-item-label>
+      </q-item-section>
+      <q-item-section v-if="task_data.done" side>
+        <q-btn
+          @click.stop="deleteTask"
+          dense
+          round
+          color="primary"
+          icon="delete"
+        />
+      </q-item-section>
+    </q-item>
+  </q-list>
 </template>
 
 <script>
